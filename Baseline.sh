3ab8208e4482c263fb5308164ab4fdcf2778cfb8
YY <ys7yoo@gmail.com>
2017-03-27 19:42:28 +0900

    modified to use Caffe RC5

--- Baseline.sh
+++ Baseline.sh
@@ -1,4 +1,11 @@
-GLOG_logtostderr=1 ./caffe-multi/build/tools/caffe train \
--gpu 1 -solver ./protofiles/solver.prototxt \
+CAFFE_ROOT=~/src/caffe-rc5
+#CAFFE_ROOT=~/src/caffe-rc3
+#CAFFE_ROOT=./caffe-multi
+
+#remove gpu option!
+# also changed solver.prototxt
+
+GLOG_logtostderr=1 ${CAFFE_ROOT}/build/tools/caffe train \
+-solver ./protofiles/solver.prototxt \
 -weights cache/VGG_ILSVRC_16_layers_full_conv.caffemodel \
 2>&1 | tee log_test.txt
