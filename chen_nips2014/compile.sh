65e7e4607231f330be0eb5b100cd1d0ba207f468
YY <ys7yoo@gmail.com>
2017-03-28 22:45:17 +0900

    script for mex

---
+++ chen_nips2014/compile.sh
@@ -0,0 +1,8 @@
+/opt/matlab/bin/mex -O -outdir bin  src/mex/distance_transform.cpp
+
+/opt/matlab/bin/mex -O -outdir bin  external/qpsolver/qp_one_sparse.cc
+
+/opt/matlab/bin/mex -O -outdir bin  external/qpsolver/score.cc
+
+/opt/matlab/bin/mex -O -outdir bin  external/qpsolver/lincomb.cc
+
