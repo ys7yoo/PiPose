6e267350e51392e3a86bd8b654de056779576691
YY <ys7yoo@gmail.com>
2017-04-24 02:13:45 +0900

    re-indent

--- chen_nips2014/src/build_model.m
+++ chen_nips2014/src/build_model.m
@@ -13,7 +13,7 @@ model.cnn.mean_pixel = compute_mean_pixel();
 model.cnn.cnn_output_dim = global_IDs{end}(end)+1;   % +1 for background
 model.cnn.step = conf.step;
 model.cnn.psize = tsize * conf.step;
-model.cnn.batch_size = conf.batch_size;              
+model.cnn.batch_size = conf.batch_size;
 
 model.tsize = tsize;
 model.global_IDs = global_IDs;
@@ -27,7 +27,7 @@ model.pdefs = struct('w',{},'i',{});               % prior of deformation (regre
 model.gaus    = struct('w',{},'i',{},'mean',{}, 'var', {});   % deformation gaussian
 
 model.components{1} = struct('parent',{}, 'pid', {}, 'nbh_IDs', {}, ...
-  'biasid',{},'appid',{},'app_global_ids',{},'pdefid',{},'gauid',{},'idpr_global_ids',{});
+    'biasid',{},'appid',{},'app_global_ids',{},'pdefid',{},'gauid',{},'idpr_global_ids',{});
 
 model.pa = pa;
 model.tsize  = model.tsize;
@@ -40,74 +40,74 @@ model.cnn = model.cnn;
 
 % add children
 for i = 1:length(pa)
-  child = i;
-  parent = pa(child);
-  assert(parent < child);
-  p.parent = parent;
-  p.pid    = child;
-  p.nbh_IDs = nbh_IDs{p.pid};
-  
-  % add bias
-  p.biasid = [];
-  if parent == 0
-    nb  = length(model.bias);
-    b.w = 0;
-    b.i = model.len + 1;
-    model.bias(nb+1) = b;
-    model.len = model.len + numel(b.w);
-    p.biasid = nb+1;
-  end
-  
-  % add appearance parameters
-  p.appid = [];
-  
-  nf  = length(model.apps);
-  f.w = 0.01;                     % encourage larger appearance score
-  f.i = model.len + 1;
-  
-  model.apps(nf+1) = f;
-  model.len = model.len + numel(f.w);
-  
-  p.appid = [p.appid, nf+1];
-  p.app_global_ids = global_IDs{i};
-  % add prior of deformation parameters
-  p.pdefid = [];
-  for nn = 1:numel(p.nbh_IDs)
-    np = length(model.pdefs);
-    pd.w = 0.01;                % encourage larger prior score
-    pd.i = model.len + 1;
+    child = i;
+    parent = pa(child);
+    assert(parent < child);
+    p.parent = parent;
+    p.pid    = child;
+    p.nbh_IDs = nbh_IDs{p.pid};
     
-    model.pdefs(np+1) = pd;
-    model.len = model.len + numel(pd.w);
-    p.pdefid(nn) = np+1;
-  end
-  % add gaussian parameters (for spring deformation)
-  p.gauid = [];
-  p.idpr_global_ids = cell(numel(p.nbh_IDs), 1);
-  for nn = 1:numel(p.nbh_IDs)
-    idpr_idx = global_IDs{i};
-    idpr_idx = permute(idpr_idx, [nn, 1:nn-1, nn+1:ndims(idpr_idx)]);
-    p.idpr_global_ids{nn} = cell(numel(clusters{p.pid}{nn}), 1);
-    for k = 1:numel(clusters{p.pid}{nn})    
-      p.idpr_global_ids{nn}{k} = idpr_idx(k,:);
-      center = clusters{p.pid}{nn}(k).center;
-      %             sigma = clusters{p.pid}{nn}(k).sigma;
-      sigma = [1,1];      % it seems 'sigma' does not improve performance
-      ng  = length(model.gaus);
-      g.w = [0.01, 0, 0.01, 0]; % [dx^2, dx, dy^2, dy]the normalization factor + variance for (x,y)
-      % res = [-dx^2, -dx, -dy^2, -dy]';
-      g.i = model.len + 1;
-      g.mean = center;
-      g.var = sigma;
-      
-      model.gaus(ng+1) = g;
-      model.len = model.len + numel(g.w);
-      p.gauid{nn}(k) = ng+1; % the kth mixture of deformation w.r.t nth neighbor
+    % add bias
+    p.biasid = [];
+    if parent == 0
+        nb  = length(model.bias);
+        b.w = 0;
+        b.i = model.len + 1;
+        model.bias(nb+1) = b;
+        model.len = model.len + numel(b.w);
+        p.biasid = nb+1;
     end
-  end
-  
-  np = length(model.components{1});
-  model.components{1}(np+1) = p;
+    
+    % add appearance parameters
+    p.appid = [];
+    
+    nf  = length(model.apps);
+    f.w = 0.01;                     % encourage larger appearance score
+    f.i = model.len + 1;
+    
+    model.apps(nf+1) = f;
+    model.len = model.len + numel(f.w);
+    
+    p.appid = [p.appid, nf+1];
+    p.app_global_ids = global_IDs{i};
+    % add prior of deformation parameters
+    p.pdefid = [];
+    for nn = 1:numel(p.nbh_IDs)
+        np = length(model.pdefs);
+        pd.w = 0.01;                % encourage larger prior score
+        pd.i = model.len + 1;
+        
+        model.pdefs(np+1) = pd;
+        model.len = model.len + numel(pd.w);
+        p.pdefid(nn) = np+1;
+    end
+    % add gaussian parameters (for spring deformation)
+    p.gauid = [];
+    p.idpr_global_ids = cell(numel(p.nbh_IDs), 1);
+    for nn = 1:numel(p.nbh_IDs)
+        idpr_idx = global_IDs{i};
+        idpr_idx = permute(idpr_idx, [nn, 1:nn-1, nn+1:ndims(idpr_idx)]);
+        p.idpr_global_ids{nn} = cell(numel(clusters{p.pid}{nn}), 1);
+        for k = 1:numel(clusters{p.pid}{nn})
+            p.idpr_global_ids{nn}{k} = idpr_idx(k,:);
+            center = clusters{p.pid}{nn}(k).center;
+            %             sigma = clusters{p.pid}{nn}(k).sigma;
+            sigma = [1,1];      % it seems 'sigma' does not improve performance
+            ng  = length(model.gaus);
+            g.w = [0.01, 0, 0.01, 0]; % [dx^2, dx, dy^2, dy]the normalization factor + variance for (x,y)
+            % res = [-dx^2, -dx, -dy^2, -dy]';
+            g.i = model.len + 1;
+            g.mean = center;
+            g.var = sigma;
+            
+            model.gaus(ng+1) = g;
+            model.len = model.len + numel(g.w);
+            p.gauid{nn}(k) = ng+1; % the kth mixture of deformation w.r.t nth neighbor
+        end
+    end
+    
+    np = length(model.components{1});
+    model.components{1}(np+1) = p;
 end
 
 
