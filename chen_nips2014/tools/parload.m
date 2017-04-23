2def1d58b1034d221b8b4bcd10bdef4c5748a33a
YY <ys7yoo@gmail.com>
2017-04-24 01:20:44 +0900

    re-indent

--- chen_nips2014/tools/parload.m
+++ chen_nips2014/tools/parload.m
@@ -1,13 +1,14 @@
-% par load
 function varargout = parload(fullpath, varargin)
+% par load
+
 varargout = cell(length(varargin), 1);
 for ii=1:length(varargin)
-  var_name = varargin{ii};
-  assert(isa(class(var_name), 'char'), 'only accept name');
-  load(fullpath, var_name);
-  if exist(var_name, 'var')
-    varargout{ii} = eval(var_name);
-  else
-    varargout{ii} = [];
-  end
+    var_name = varargin{ii};
+    assert(isa(class(var_name), 'char'), 'only accept name');
+    load(fullpath, var_name);
+    if exist(var_name, 'var')
+        varargout{ii} = eval(var_name);
+    else
+        varargout{ii} = [];
+    end
 end
